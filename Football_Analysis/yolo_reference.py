6acf3bbaba981eaa442106fab0a3de8c976d640a
R4chC0dE <140041789+R4chC0dE@users.noreply.github.com>
2024-09-25 21:21:59 +1000

    updated gitignore file to ignore .pt files required to run the YOLO model

--- Football_Analysis/yolo_reference.py
+++ Football_Analysis/yolo_reference.py
@@ -1,5 +1,12 @@
 from ultralytics import YOLO
 
 model = YOLO('yolov8x')
+# model.to('cuda')
 
-model.predict('input_video/')
+result = model.predict('input_video/08fd33_4.mp4', save=True)
+
+print(result[0])
+print('================')
+
+for box in result[0].boxes:
+    print(box)
