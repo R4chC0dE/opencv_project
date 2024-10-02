a9b0dbec060dd82cb6c500a23627db78d09ce5c9
R4chC0dE <140041789+R4chC0dE@users.noreply.github.com>
2024-10-02 16:56:51 +1000

    moved files around

--- Football_Analysis/main.py
+++ Football_Analysis/main.py
@@ -1,9 +1,14 @@
 from utils.video_utils import read_video, save_video
+from trackers.tracker import Tracker
 
 def main():
     # read video 
     video_frames = read_video('input_videos/08fd33_4.mp4')
 
+    # initialise tracker
+    tracker = Tracker('models/best.pt')
+
+    tracks = tracker.get_object_tracks(video_frames)
     # Save video
     save_video(video_frames, 'output_videos/output_video.avi')
 
